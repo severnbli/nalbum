ae2d6f398e8fe56eab9c83bf65ea988114087063
Usevalad Buben <usevaladbuben@gmail.com>
2026-06-09 00:37:39 +0300

    Create BaseTag, ITag inheritor

---
+++ nAlbum/Assets/_Project/Scripts/Runtime/Features/Tags/BaseTag.cs
@@ -0,0 +1,21 @@
+﻿/*
+    nAlbum – Media album based on Unity 
+    Copyright (C) 2026 Severnbli (aka Usevalad Buben)
+    
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using System.Collections.Generic;
+
+namespace Severnbli.NAlbum.Runtime.Features.Tags
+{
+    public class BaseTag : ITag
+    {
+        private string _name;
+        
+        public HashSet<ITag> Children { get; } = new();
+
+        public virtual string GetDescription() => _name;
+    }
+}
