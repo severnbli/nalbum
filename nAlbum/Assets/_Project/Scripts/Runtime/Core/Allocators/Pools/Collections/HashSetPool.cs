bcba2bb4ff0d85b0cd9ee66b0c8d832acd9cf574
Usevalad Buben <usevaladbuben@gmail.com>
2026-06-11 01:29:17 +0300

    Create basic collections pools definitions: DictionaryPool, HashSetPool, ListPool, QueuePool and StackPool

---
+++ nAlbum/Assets/_Project/Scripts/Runtime/Core/Allocators/Pools/Collections/HashSetPool.cs
@@ -0,0 +1,20 @@
+﻿/*
+    nAlbum – Media album based on Unity 
+    Copyright (C) 2026 Severnbli (aka Usevalad Buben)
+    
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using System.Collections.Generic;
+
+namespace Severnbli.NAlbum.Runtime.Core.Allocators.Pools.Collections
+{
+    public class HashSetPool<T> : Pool<HashSet<T>>
+    {
+        protected override HashSet<T> CreateInstance()
+        {
+            return new HashSet<T>(PoolsContracts.CollectionPoolInitSize);
+        }
+    }
+}
