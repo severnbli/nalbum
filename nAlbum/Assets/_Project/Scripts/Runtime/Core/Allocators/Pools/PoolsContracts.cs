2192a4c555b93227566a5bde66b2f8d51406b30d
Usevalad Buben <usevaladbuben@gmail.com>
2026-06-11 01:21:09 +0300

    Create PoolsContracts with only contract "CollectionPoolInitSize" with value "5"

---
+++ nAlbum/Assets/_Project/Scripts/Runtime/Core/Allocators/Pools/PoolsContracts.cs
@@ -0,0 +1,15 @@
+﻿/*
+    nAlbum – Media album based on Unity 
+    Copyright (C) 2026 Severnbli (aka Usevalad Buben)
+    
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+namespace Severnbli.NAlbum.Runtime.Core.Allocators.Pools
+{
+    public static class PoolsContracts
+    {
+        public const int CollectionPoolInitSize = 5;
+    }
+}
