c79afc12b5f19b158414e60f0466b7614ac1df77
Usevalad Buben <usevaladbuben@gmail.com>
2026-06-11 23:32:37 +0300

    Create LogUtils with "Log", "LogWarning" and "LogError" methods with "Conditional" attribute

---
+++ nAlbum/Assets/_Project/Scripts/Runtime/Shared/Utils/LogUtils.cs
@@ -0,0 +1,34 @@
+﻿/*
+    nAlbum – Media album based on Unity
+    Copyright (C) 2026 Severnbli (aka Usevalad Buben)
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using System.Diagnostics;
+using Debug = UnityEngine.Debug;
+
+namespace Severnbli.NAlbum.Runtime.Shared.Utils
+{
+    public static class LogUtils
+    {
+        [Conditional("DEBUG")]
+        public static void Log(string msg)
+        {
+            Debug.Log(msg);
+        }
+        
+        [Conditional("DEBUG")]
+        public static void LogWarning(string msg)
+        {
+            Debug.LogWarning(msg);
+        }
+        
+        [Conditional("DEBUG")]
+        public static void LogError(string msg)
+        {
+            Debug.LogError(msg);
+        }
+    }
+}
