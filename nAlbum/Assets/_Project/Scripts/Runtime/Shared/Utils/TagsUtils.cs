ebd6c91805c83cdef7af8b5a63166bb801802373
Usevalad Buben <usevaladbuben@gmail.com>
2026-06-11 23:50:23 +0300

    Create TagsUtils with only method "DoActionWithTags"

---
+++ nAlbum/Assets/_Project/Scripts/Runtime/Shared/Utils/TagsUtils.cs
@@ -0,0 +1,25 @@
+﻿/*
+    nAlbum – Media album based on Unity
+    Copyright (C) 2026 Severnbli (aka Usevalad Buben)
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Collections.Generic;
+using Severnbli.NAlbum.Runtime.Features.Tags;
+
+namespace Severnbli.NAlbum.Runtime.Shared.Utils
+{
+    public static class TagsUtils
+    {
+        public static void DoActionWithTags(HashSet<ITag> tags, Action<ITag> action)
+        {
+            foreach (var tag in tags)
+            {
+                action(tag);
+            }
+        }
+    }
+}
